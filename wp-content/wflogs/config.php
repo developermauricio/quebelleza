31e6fe5ee3f36f613fdde958aeb39e94fa829cb7
Mauricio Gutierrez <developer.mauricio2310@gmail.com>
2022-04-04 17:21:26 +0000

    Ajustes

--- wp-content/wflogs/config.php
+++ wp-content/wflogs/config.php
@@ -3,4 +3,4 @@
 This file is used by the Wordfence Web Application Firewall. Read 
 more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
 ******************************************************************
-a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1639603725;s:7:"authKey";s:64:"dxy8Yq8%ejy?1Sji|2h]u)r-xVI#)^CbDYXNp<u;7jk){ZMA h:9c=KPxIEe 6]!";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:2439;}
+a:6:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:"dxy8Yq8%ejy?1Sji|2h]u)r-xVI#)^CbDYXNp<u;7jk){ZMA h:9c=KPxIEe 6]!";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:2439;}
